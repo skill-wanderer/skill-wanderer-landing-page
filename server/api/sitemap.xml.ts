dc0049cd94507654662f2e39c9c9ff76c255daf1
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2025-06-23 20:50:26 +0700

    fix http

--- server/api/sitemap.xml.ts
+++ server/api/sitemap.xml.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://skill-wanderer.com/</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
