a474771ef84d38ef2c4cd1cdc09b344d7d9387f0
Thanh-Nguyen-Valerie <thanh.nguyen@skill-wanderer.com>
2026-03-23 10:05:30 +0700

    feat: add mobile development learning path page

---
+++ pages/learning-path/mobile-development.vue
@@ -0,0 +1,16 @@
+<template>
+  <ReusablePathOverview :overview="overview" />
+</template>
+
+<script setup lang="ts">
+import ReusablePathOverview from '~/components/learning-path/ReusablePathOverview.vue'
+import { reusablePathOverviews } from '~/data/learning-path-overviews'
+
+const overview = reusablePathOverviews['mobile-development']
+
+useSEO({
+  title: `${overview.title} Learning Path | Skill-Wanderer`,
+  description: overview.description,
+  keywords: ['mobile development', 'learning path', 'skill-wanderer', 'coming soon']
+})
+</script>
