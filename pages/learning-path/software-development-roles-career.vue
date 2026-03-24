184305f6adebf60b8db3fddbf2f99a2085e6ade8
Thanh-Nguyen-Valerie <thanh.nguyen@skill-wanderer.com>
2026-03-24 10:29:01 +0700

    feat: add software development roles & career learning path page

---
+++ pages/learning-path/software-development-roles-career.vue
@@ -0,0 +1,22 @@
+<template>
+  <ReusablePathOverview :overview="overview" />
+</template>
+
+<script setup lang="ts">
+import ReusablePathOverview from '~/components/learning-path/ReusablePathOverview.vue'
+import { reusablePathOverviews } from '~/data/learning-path-overviews'
+
+const overview = reusablePathOverviews['software-development-roles-career']
+
+useSEO({
+  title: `${overview.title} Learning Path | Skill-Wanderer`,
+  description: overview.description,
+  keywords: [
+    'software development career path',
+    'software engineering roles roadmap',
+    'tech career growth',
+    'developer career guide',
+    'skill wanderer software development roles career'
+  ]
+})
+</script>
