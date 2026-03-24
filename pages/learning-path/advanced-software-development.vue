400bfae79f7670be81b9f9d99da0b69a019dae30
Thanh-Nguyen-Valerie <thanh.nguyen@skill-wanderer.com>
2026-03-24 10:32:04 +0700

    feat: add advanced software development learning path page

---
+++ pages/learning-path/advanced-software-development.vue
@@ -0,0 +1,22 @@
+<template>
+  <ReusablePathOverview :overview="overview" />
+</template>
+
+<script setup lang="ts">
+import ReusablePathOverview from '~/components/learning-path/ReusablePathOverview.vue'
+import { reusablePathOverviews } from '~/data/learning-path-overviews'
+
+const overview = reusablePathOverviews['advanced-software-development']
+
+useSEO({
+  title: `${overview.title} Learning Path | Skill-Wanderer`,
+  description: overview.description,
+  keywords: [
+    'advanced software development learning path',
+    'clean code solid principles',
+    'system design performance engineering',
+    'technical leadership roadmap',
+    'skill wanderer advanced software development'
+  ]
+})
+</script>
