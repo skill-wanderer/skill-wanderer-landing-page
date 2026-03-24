d940992ed75babd78cf3b5064a2682b62100e28b
Thanh-Nguyen-Valerie <thanh.nguyen@skill-wanderer.com>
2026-03-24 10:27:46 +0700

    feat: add mlops learning path page

---
+++ pages/learning-path/mlops.vue
@@ -0,0 +1,22 @@
+<template>
+  <ReusablePathOverview :overview="overview" />
+</template>
+
+<script setup lang="ts">
+import ReusablePathOverview from '~/components/learning-path/ReusablePathOverview.vue'
+import { reusablePathOverviews } from '~/data/learning-path-overviews'
+
+const overview = reusablePathOverviews['mlops']
+
+useSEO({
+  title: `${overview.title} Learning Path | Skill-Wanderer`,
+  description: overview.description,
+  keywords: [
+    'mlops learning path',
+    'machine learning operations roadmap',
+    'ml pipeline deployment monitoring',
+    'model serving experiment tracking',
+    'skill wanderer mlops'
+  ]
+})
+</script>
