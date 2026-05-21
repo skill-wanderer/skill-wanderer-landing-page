a346a7b3f0f4c809ad40f75e03da8dd197c44866
DuyAnh <duyanhdao297@gmail.com>
2026-05-21 10:17:23 +0700

    fix: open dojo and blog links in new tab in footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -5,8 +5,8 @@
         <NuxtLink to="/about">About</NuxtLink>
         <NuxtLink to="/principles">Guild Principles</NuxtLink>
         <NuxtLink to="/work-with-us">Engage the Guild</NuxtLink>
-        <NuxtLink to="https://dojo.skill-wanderer.com">Dojo</NuxtLink>
-        <NuxtLink to="https://wanderings.skill-wanderer.com">Blog</NuxtLink>
+        <a href="https://dojo.skill-wanderer.com" target="_blank" rel="noopener noreferrer">Dojo</a>
+        <a href="https://wanderings.skill-wanderer.com" target="_blank" rel="noopener noreferrer">Blog</a>
         <a href="https://linkedin.com/company/skill-wanderer" target="_blank" rel="noopener noreferrer">Company LinkedIn</a>
         <NuxtLink to="/contact">Contact</NuxtLink>
         <NuxtLink to="/privacy-policy">Privacy Policy</NuxtLink>
